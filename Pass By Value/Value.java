0dd1388ff3034dc336ba4a3339c955a88416583e
jonathanngwata <ngwatajonathan@gmail.com>
2025-11-23 11:05:57 +0200

    Pass by value implementation

---
+++ Pass By Value/Value.java
@@ -0,0 +1,30 @@
+//Java code to implement pass by value
+
+public class Value{
+
+	public static void value(int original_value){
+
+		original_value = 34;
+
+		System.out.println("Original Value = " + original_value);
+	}
+
+	public static void main(String args[]){
+		Value v = new Value();
+
+		int test_value = 33;
+
+		System.out.println("Before method call, test value " + test_value);
+
+		value(test_value);
+
+		System.out.println("After method call, test value =" + test_value);
+
+
+
+	}
+
+
+
+}
+
